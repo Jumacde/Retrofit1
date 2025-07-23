011598fbab74773544f6cf5c85bd23b3e0db2b41
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-23 21:58:12 +0200

    Commited

--- app/src/main/java/com/example/retrofit1/ResponseData.java
+++ app/src/main/java/com/example/retrofit1/ResponseData.java
@@ -1,4 +1,13 @@
 package com.example.retrofit1;
 
 public class ResponseData {
+
+
+    private String body;
+
+    public ResponseData() {
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/retrofit1/RetrofitInterface.java
@@ -0,0 +1,4 @@
+package com.example.retrofit1;
+
+public interface RetrofitInterface {
+}
